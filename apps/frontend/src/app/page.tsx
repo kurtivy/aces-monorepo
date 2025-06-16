0a395630945a5016c21a03ef9ed87344b6b11dc6
rahmsc <danmilad.code@gmail.com>
2025-06-16 13:03:23 +0200

    Removed overflow hidden from canvas

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -4,7 +4,7 @@ import InfiniteCanvas from '../components/canvas/infinite-canvas';
 
 export default function Home() {
   return (
-    <main className="w-full h-screen overflow-hidden bg-black">
+    <main className="w-full h-screen bg-black">
       <InfiniteCanvas />
     </main>
   );
