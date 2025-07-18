34dad343719fbd4a3c0637879eb992689d155d1a
rahmsc <danmilad.code@gmail.com>
2025-07-18 16:00:41 +0200

    refactor: remove old launch components
    - Remove deprecated launch page and related components
    - Clean up old bonding curve chart implementation
    - Remove outdated launch swap interface
    - Delete old token launch page components
    - Consolidate launch functionality into new ICO system

--- apps/frontend/src/app/launch/page.tsx
+++
@@ -1,5 +0,0 @@
-import TokenLaunchPage from '@/components/launch/token-launch-page';
-
-export default function LaunchPage() {
-  return <TokenLaunchPage tokenSymbol="RWA" tokenName="Real World Assets" />;
-}
