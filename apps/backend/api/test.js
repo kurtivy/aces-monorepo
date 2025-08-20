17150c3d9b4d3062f6c1ba7e02b5c31c079b2792
rahmsc <danmilad.code@gmail.com>
2025-08-20 19:20:16 +0200

    Removed test.js

--- apps/backend/api/test.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res) => {
-  res.status(200).json({
-    message: 'Hello from direct API test!',
-    method: req.method,
-    url: req.url,
-    timestamp: new Date().toISOString(),
-  });
-};
