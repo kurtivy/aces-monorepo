81f1090aaa836c924450047e5696cd77a9fd9e2a
rahmsc <danmilad.code@gmail.com>
2026-01-21 08:52:11 +0700

    Added Farcaster mini app support Added generateMetadata for Farcaster mini app instead of Metadata

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -2,8 +2,15 @@
 
 import InfiniteCanvas from '../components/canvas/infinite-canvas';
 import { CanvasErrorBoundary } from '../components/canvas-error-boundary';
+import { useEffect } from 'react';
+import { sdk } from '@farcaster/miniapp-sdk';
+
 
 export default function Home() {
+  useEffect(() => {
+    sdk.actions.ready();
+}, []);
+
   return (
     <main className="w-full h-screen bg-black">
       <CanvasErrorBoundary>
