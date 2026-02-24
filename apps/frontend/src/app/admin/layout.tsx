e12e689fc73fbe2ff8ac2aa50c171cd18a08500b
rahmsc <danmilad.code@gmail.com>
2026-02-24 11:48:57 +0700

    Added commment

--- apps/frontend/src/app/admin/layout.tsx
+++ apps/frontend/src/app/admin/layout.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+//home
 import { AdminAuthProvider } from '@/lib/auth/admin-auth-context';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useState } from 'react';
